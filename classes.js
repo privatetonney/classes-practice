7b3b0115ade2832949020d39909c4a5f3156e4bf
tonny kipkosgei <tonykipkosgei@lumen.local>
2019-07-11 21:47:14 +0300

    first commit

---
+++ classes.js
@@ -0,0 +1,15 @@
+class shape {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+	move() {}
+}
+
+class rectangle extends shape {
+	constructor(x, y, height, widht) {
+		super(x, y);
+		this.width = width;
+		this.height = height;
+	}
+}
